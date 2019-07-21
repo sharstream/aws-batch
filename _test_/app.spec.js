86f5e302ffe1e2fb564ee4c53ce5565c38e59d8a
David Perez <kitsunebrave@gmail.com>
2019-07-21 19:31:10 -0400

    s3 mock

--- _test_/app.spec.js
+++ _test_/app.spec.js
@@ -1,8 +1,18 @@
 'use strict';
 
-const s3Object = require('../app').s3Object;
+let s3Object = require('../app.js');
+jest.mock('../app');
 
 jest.fn().mockImplementation(() => Promise.resolve(value));
+
+test('gets metadata', async () => {
+  const s3 = new s3Object('test-bucket', 'test-key');
+  s3.getMetadata.mockResolvedValue({ metadata: 'AWS'})
+  const metadata = s3.getMetadata();
+  console.log(metadata)
+  // expect(metadata).toHaveBeenCalled();
+  expect(metadata).resolves.toEqual({metadata: 'AWS'});
+})
 
 test('async test', async () => {
   const asyncMock = jest.fn().mockResolvedValue(43);
--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //app.js
+'use strict';
+
 const S3 = require('aws-sdk/clients/s3');
 const s3 = new S3();
 const API_VERSION = process.env.API_VERSION;
@@ -11,11 +13,14 @@ class s3Object {
      * Instantiate S3 on the instance; future-proof to allow per-object config
      * changes.
      */
-    this.s3 = new AWS.S3({ apiVersion: API_VERSION });
+    this.s3 = new S3({ apiVersion: API_VERSION });
     this.bucket = bucket;
     this.key = key;
   }
 
+  /**
+   * get metadata
+   */
   getMetadata() {
     const params = {
       Bucket: this.bucket,
@@ -23,13 +28,16 @@ class s3Object {
     }
     new Promise((resolve, reject) => {
       s3.putObject(params, (err, data) => {
-          if (err) {
-              reject(err)
-          } else {
-             resolve(data)
-         }
+        if (err) {
+          reject(err)
+        } else {
+          resolve(data.Metadata)
+        }
       })
     })
   }
 }
-module.exports = { s3Object };
+module.exports = s3Object;
+
+// let classObj = new s3Object('test', 'key');
+// console.log('building class...', classObj)
