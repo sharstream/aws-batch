d549a58bc3d3a4fb46e3dff226b10787a4e8d8c3
David Perez <kitsunebrave@gmail.com>
2019-08-18 18:29:29 -0400

    sending request

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+// spawn a child process and execute shell command
+
 'use strict';
 
 const http = require('http')
+    , util = require('util')
+    , delay = util.promisify(setTimeout)
     , { platform } = require('os')
     , osType = platform()
 // The path to the .bat files
@@ -12,35 +16,49 @@ const port = process.env.PORT || 8080
     , MAX_CHILDREN = 3
     , argsArray = [`${batches}/install.bat`, `${batches}/test.bat`, `${batches}/reset.bat`];
 
-const run = async (cmd, argsArray) => {
+const run = async (cmd) => {
     // exec all scripts spaces/separated by each .bat filename:
     const spawn = require('child_process').spawn;
-    const children = spawn(cmd, [
+    const excluded = spawn(cmd, [
     '-o', `${batches}/install.bat`,
     '-o', `${batches}/test.bat`,
     '-o', `${batches}/reset.bat`
     ], { shell: true });//convert from stream to buffer);
-    // or: need to set some timeout between each .bat file
     // use spawn to retreive huge binary data to Node and avoid Buffer Error: maxBuffer exceeded
-
+    
     return new Promise(async (resolve, reject) => {
+        
+        //initialize stream batch
+        process.stdout.write(`Sending ${MAX_CHILDREN} requests...`)
 
-        let buffer = Buffer.from(); //initialize buffer
+        let children = []
+        for (let index = 0; index < MAX_CHILDREN; index++) {
+            children.push(spawn(cmd, [`${batches}/install.bat`], { shell: true }))
+        }
         let resps = children.map(child => {
             return new Promise((resolve, reject) => {
                 child.on('exit', function(code){
-                    if (code === 0) resolve(true)
-                    reject(false)
-                })
+                    try { 
+                        if (code === 0) resolve(true)
+                    } catch (error) {
+                        let err = new Error(`command ${cmd} exited with wrong status code ${code} and error message: ${error.message}`);
+                        reject(err)
+                    }
+                }) 
             })
         })
-
+        
         resps = await Promise.all(resps)
-
+        
         if (resps.filter(Boolean).length === MAX_CHILDREN ) {
             console.log(`success!`)
-        } else console.log(`failures.`)
-
+            resolve({ success: resps })
+        } else {
+            console.log(`failures.`)
+            reject({ error: 'failures' })
+        }
+        
+        // or: need to set some timeout between each .bat file
         await delay(500);
     })
 }
@@ -48,7 +66,7 @@ const run = async (cmd, argsArray) => {
 const server = http.createServer( async (req, res) => {
     res.writeHead(200, {'Content-Type': 'application/json'});
     //execute .bat in async way
-    const success = await run('start');
+    let success = osType !== 'win32' ? await run('start') : new Error(`shell command is not compatible \n`);
     let msg = 'batch success: ' + success;
 
     res.end(msg + '\\n'); console.log(msg);
