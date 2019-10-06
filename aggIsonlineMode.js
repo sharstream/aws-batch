5f04a4f640648ed3ed59e1041cb9913394740000
David Perez <kitsunebrave@gmail.com>
2019-10-06 01:11:20 -0400

    mutation using spread operator

---
+++ aggIsonlineMode.js
@@ -0,0 +1,32 @@
+const getIsolineMode = (query) => {
+
+    const query = {
+        "car": "",
+        "mode": "fastest",
+        "range": "1800",
+        "rangetype": "time",
+        "start": "geo!33.83752,-84.3757399",
+        "traffic:enabled": ""
+    }
+    
+    const arr = Object.entries(query)
+    let setMode = "";
+    for (const [mode, value] of arr) {
+        value === '' ? console.log(`value of ${mode} is ${value} empty`) : console.log(`value isn't empty`)
+        value === ''  ? setMode = setMode.concat(',', mode) : 'Hello'
+    }
+    const isolineObj = Object.keys(query).reduce((obj, key) => {
+        
+        //TODO aggregate all empty attributes to setMode var and return a new property: mode
+        let aggMode;
+        if(key === "mode") {
+            aggMode = query[key]
+            aggMode = aggMode.concat('', setMode)
+        }
+        key === "mode" ? obj[key] = aggMode : query[key] !== '' ? obj[key] = query[key] : null
+        return obj
+    }, {})
+    
+    console.log(isolineObj)
+    return isolineObj
+}
