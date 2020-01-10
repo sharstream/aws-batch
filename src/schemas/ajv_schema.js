4b5e1652b2f23e5f61e5adae8990633f403da8fa
Dave Perez <kitsunebrave@gmail.com>
2020-01-09 23:32:25 -0500

    ajv constructor and format

--- src/schemas/ajv_schema.js
+++ src/schemas/ajv_schema.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Ajv = require('ajv');
-const ajv = new Ajv({ allErrors: extras, verbose: extras });
+const ajv = new Ajv({ $data: true, allErrors: true, verbose: true });
 require('ajv-keywords')(ajv);
 
 const event_schema = {
@@ -9,19 +9,19 @@ const event_schema = {
         type: 'object',
         required: ['status', 'message'],
         properties: {
+            status: {
+                type: 'string'
+            },
             message: {
                 type: 'string'
             },
-            status: {
-                type: 'string'
-            }
         }
     }
 }
 
 const valdiate_events = events => {
     const invalid_events = [];
-    valid_events = events.filter(event => {
+    const valid_events = events.filter(event => {
         const valid = ajv.validate(event_schema, event);
         if(!valid) {
             const failed_schema = {
@@ -41,7 +41,7 @@ const valdiate_events = events => {
         return valid;
     })
 
-    const finalEvents = { invalidEvents: null, validEvents: valid_events };
+    const finalEvents = { invalidEvents: [], validEvents: valid_events };
     if(invalid_events.length > 0) {
         finalEvents.invalidEvents = invalid_events;
     }
@@ -53,18 +53,18 @@ const push_sqs_event_to_kinesis = async (events, opts) => {
     const { invalidEvents, validEvents } = valdiate_events(events);
 
     const finalErrorResponse = {
-        kinesis_errors = null,
-        validation_errors = null
+        kinesis_errors: null,
+        validation_errors: null
     };
 
-    noValidalidEventsExist = validEvents.length === 0;
-    validEventsExist = validEvents.length > 0;
+    let noValidalidEventsExist = validEvents.length === 0,
+        validEventsExist = invalidEvents.length === 0;
 
     if(noValidalidEventsExist) throw finalErrorResponse;
 
     else {
         try {
-            const response = await push_message_kinesis(invalidEvents, opts);
+            const response = await push_message_kinesis(validEvents, opts);
             if(validEventsExist) return response;
         } catch (error) {
             if(error instanceof Error) {
@@ -85,3 +85,19 @@ const push_sqs_event_to_kinesis = async (events, opts) => {
 }
 
 module.exports = { push_sqs_event_to_kinesis };
+// const opts = {
+//     region: 'us-east-1',
+//     retry: true,
+//     streamName: 'kinesis-stream-name',
+//     bucket: 'kinesis-s3-bucket'
+// },
+// events = [
+//     {
+//         data: {
+//             status: 'success',
+//             message: 'testing schema validation',
+//             Records: [{ count: 1 }]
+//         }
+//     }
+// ]
+// push_sqs_event_to_kinesis(events, opts)
